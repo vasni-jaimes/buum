caf0436ad86b8da32732a1d3d3e35a8a9aa93134
Vasni Jaimes <vasnijaimes@Vasnis-MacBook-Pro.local>
2021-03-08 06:29:24 -0600

    front

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,17 +27,31 @@ if (itemMenu) {
 
 
 
-//EFECTO HOME SPRINNG
+//REDIRECCION SUMMER
+const summer = document.querySelector('.info-slider .estacion-principal');
+if (summer) {
+	summer.addEventListener('click', function(e) {
+		e.preventDefault();
+		window.location.href = '/buum/summer.html';
+	});
+}
+
+
+
+//EFECTO HOME H2 -(PRINCIPAL)
 $('section.slider-bumming .info-slider h2:nth-child(2)').hover(function(){
 	$(this).css('letter-spacing', '25px');
+	$(this).css('display', 'inline-block');
 	$(this).addClass('cambio');
 	$(this).prev().css({
 		'letterSpacing': '25px',
 		'color': '#D24D39',
 	});
 
-	$('figure.one-img').fadeOut();
-	$('figure.two-img').fadeIn();
+	// $('figure.one-img').fadeOut();
+	// $('figure.two-img').fadeIn();
+	$('figure.one-img').css("display", "none");
+	$('figure.summer').css("display", "block");
 
 }, function(){
 
@@ -48,8 +62,50 @@ $('section.slider-bumming .info-slider h2:nth-child(2)').hover(function(){
 		'color': '#000',
 	});
 
-	$('figure.one-img').fadeIn();
-	$('figure.two-img').fadeOut();
+	// $('figure.one-img').fadeIn();
+	// $('figure.two-img').fadeOut();
+
+	$('figure.summer').css("display", "none");
+	$('figure.one-img').css("display", "block");
+	
+});
+
+
+
+//EFECTO HOME SPRINNG
+$('section.slider-bumming .info-slider ul li.spring').hover(function(){
+	let hh2 = $('section.slider-bumming .info-slider h2:nth-child(2)');
+
+	hh2.text("SPRING");
+	hh2.css('letter-spacing', '25px');
+	hh2.css('display', 'inline-block');
+	hh2.addClass('cambio');
+	hh2.prev().css({
+		'letterSpacing': '25px',
+		'color': '#D24D39',
+	});
+
+	// $('figure.one-img').fadeOut();
+	// $('figure.two-img').fadeIn();
+	$('figure.one-img').css("display", "none");
+	$('figure.two-img').css("display", "block");
+
+}, function(){
+
+	let hh2 = $('section.slider-bumming .info-slider h2:nth-child(2)');
+	hh2.text("SUMMER");
+    hh2.css('letter-spacing', '0');
+    hh2.removeClass('cambio');
+	hh2.prev().css({
+		'letterSpacing': '0',
+		'color': '#000',
+	});
+
+	// $('figure.one-img').fadeIn();
+	// $('figure.two-img').fadeOut();
+
+	$('figure.two-img').css("display", "none");
+	$('figure.one-img').css("display", "block");
 });
 
 
@@ -59,14 +115,19 @@ $('section.slider-bumming .info-slider ul li.summer').hover(function(){
 
 	hh2.text("SUMMER");
 	hh2.css('letter-spacing', '25px');
+	hh2.css('display', 'inline-block');
 	hh2.addClass('cambio');
 	hh2.prev().css({
 		'letterSpacing': '25px',
 		'color': '#D24D39',
 	});
 
-	$('figure.one-img').fadeOut();
-	$('figure.summer').fadeIn();
+	// $('figure.one-img').fadeOut();
+	// $('figure.summer').fadeIn();
+
+	$('figure.one-img').css("display", "none");
+	$('figure.summer').css("display", "block");
+
 }, function(){
 	let hh2 = $('section.slider-bumming .info-slider h2:nth-child(2)');
 
@@ -78,8 +139,12 @@ $('section.slider-bumming .info-slider ul li.summer').hover(function(){
 		'color': '#000',
 	});
 
-	$('figure.one-img').fadeIn();
-	$('figure.summer').fadeOut();
+	// $('figure.one-img').fadeIn();
+	// $('figure.summer').fadeOut();
+
+	$('figure.summer').css("display", "none");
+	$('figure.one-img').css("display", "block");
+	
 });
 
 
@@ -91,27 +156,37 @@ $('section.slider-bumming .info-slider ul li.fall').hover(function(){
 
 	hh2.text("FALL");
 	hh2.css('letter-spacing', '25px');
+	hh2.css('display', 'block');
 	hh2.addClass('cambio');
 	hh2.prev().css({
 		'letterSpacing': '25px',
 		'color': '#D24D39',
 	});
 
-	$('figure.one-img').fadeOut();
-	$('figure.fall').fadeIn();
+	// $('figure.one-img').fadeOut();
+	// $('figure.fall').fadeIn();
+
+	$('figure.one-img').css("display", "none");
+	$('figure.fall').css("display", "block");
+
 }, function(){
 	let hh2 = $('section.slider-bumming .info-slider h2:nth-child(2)');
 
-	hh2.text("SPRING");
+	hh2.text("SUMMER");
     hh2.css('letter-spacing', '0');
+    hh2.css('display', 'inline-block');
     hh2.removeClass('cambio');
 	hh2.prev().css({
 		'letterSpacing': '0',
 		'color': '#000',
 	});
 
-	$('figure.one-img').fadeIn();
-	$('figure.fall').fadeOut();
+	// $('figure.one-img').fadeIn();
+	// $('figure.fall').fadeOut();
+
+	$('figure.fall').css("display", "none");
+	$('figure.one-img').css("display", "block");
+	
 });
 
 
@@ -122,18 +197,23 @@ $('section.slider-bumming .info-slider ul li.winter').hover(function(){
 
 	hh2.text("WINTER");
 	hh2.css('letter-spacing', '25px');
+	hh2.css('display', 'inline-block');
 	hh2.addClass('cambio');
 	hh2.prev().css({
 		'letterSpacing': '25px',
 		'color': '#D24D39',
 	});
 
-	$('figure.one-img').fadeOut();
-	$('figure.winter').fadeIn();
+	// $('figure.one-img').fadeOut();
+	// $('figure.winter').fadeIn();
+
+	$('figure.one-img').css("display", "none");
+	$('figure.winter').css("display", "block");
+
 }, function(){
 	let hh2 = $('section.slider-bumming .info-slider h2:nth-child(2)');
 
-	hh2.text("SPRING");
+	hh2.text("SUMMER");
     hh2.css('letter-spacing', '0');
     hh2.removeClass('cambio');
 	hh2.prev().css({
@@ -141,8 +221,12 @@ $('section.slider-bumming .info-slider ul li.winter').hover(function(){
 		'color': '#000',
 	});
 
-	$('figure.one-img').fadeIn();
-	$('figure.winter').fadeOut();
+	// $('figure.one-img').fadeIn();
+	// $('figure.winter').fadeOut();
+
+	$('figure.winter').css("display", "none");
+	$('figure.one-img').css("display", "block");
+	
 });
 
 
